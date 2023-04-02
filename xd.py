ff1cb5bafe12e08d3df08ba6ae5e944274af428d
Aaron <114060853+hecabre@users.noreply.github.com>
2023-04-02 15:33:33 -0600

    Delete xd.py

--- xd.py
+++
@@ -1,9 +0,0 @@
-boletas = []
-for i in range(0, 3):
-    boleta = str(input('Introduce el numero de boleta: '))
-    boletas.append(boleta)
-
-print(boletas)
-boletas[2] = input('Cual es el nombre que estara en la tercera posicion: ')
-print(boletas)
-boletas.clear()
